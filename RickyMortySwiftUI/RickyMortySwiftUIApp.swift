5f974341f355ec28bc18fd8534b9d52627c8dc9b
Serhat Yegin <yeginserhat938@gmail.com>
2024-11-09 18:07:23 +0300

    Initial Commit

---
+++ RickyMortySwiftUI/RickyMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyMortySwiftUIApp.swift
+//  RickyMortySwiftUI
+//
+//  Created by serhat on 9.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickyMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
